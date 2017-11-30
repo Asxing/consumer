777b2bbf4f9b9482eb661fa3a9bafb45c8795493
holddie <holddie@163.com>
2017-11-30 14:47:36 +0800

    Hystirxboard 配置

--- src/main/java/com/holddie/cloud/rpc/consumer/ConsumerApplication.java
+++ src/main/java/com/holddie/cloud/rpc/consumer/ConsumerApplication.java
@@ -2,15 +2,19 @@ package com.holddie.cloud.rpc.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 }
